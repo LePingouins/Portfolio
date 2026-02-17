3fa27085574a09e920f1532bf1593d3186b625c0
Olivier Goudreault <oligoudreault@gmail.com>
2026-02-17 00:43:05 -0500

    Added fix

--- backend/src/main/java/com/olivier/portfolio/SecurityConfig.java
+++ backend/src/main/java/com/olivier/portfolio/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://portfolio-frontend-iy8v.onrender.com"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
