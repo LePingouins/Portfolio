3b3e72daf1341a8aab81770464af76112f04ae65
Olivier Goudreault <oligoudreault@gmail.com>
2026-02-17 12:14:49 -0500

    Added API

--- backend/src/main/java/com/olivier/portfolio/HelloController.java
+++ backend/src/main/java/com/olivier/portfolio/HelloController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Backend is running!";
+    }
+
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from the backend!";
